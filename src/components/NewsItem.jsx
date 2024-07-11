371833c60d95206fb8458dd065de021553895855
kubat <kubat.aiupov@gmail.com>
2024-07-11 11:21:11 +0200

    add edit card

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -3,24 +3,37 @@ import { Card, Button, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const NewsItem = (props) => {
-  const { title, urlToImage, description, url, id, onShow } = props;
+  const { title, url, id, onShow } = props;
+  let { urlToImage } = props;
 
-  const shortendDescription = (description && description.slice(0, 30)) || null;
-  console.log(shortendDescription);
+  const placeholderImg =
+    "https://images.unsplash.com/photo-1599009434802-ca1dd09895e7?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  if (!urlToImage) {
+    urlToImage = placeholderImg;
+  }
+
+  const shortenedTitle = (title && title.slice(0, 81)) || null;
+
   return (
     <Card
       className="text-bg-dark mb-3"
       style={{ maxWidth: "18rem", height: "30rem" }}
     >
-      <Card.Img variant="top" src={urlToImage} />
+      <Card.Img
+        variant="top"
+        src={urlToImage}
+        style={{ height: "10rem", width: "auto", objectFit: "cover" }}
+      />
       <Card.Body>
         <Card.Title className="text-center m-2">{title}</Card.Title>
-        <Card.Text className="m-2 p-2">{shortendDescription}</Card.Text>
+        {/* <Card.Text className="m-2 p-2">{shortendDescription}</Card.Text> */}
         <div
           style={{
             display: "flex",
             justifyContent: "center",
             gap: "1rem",
+            marginTop: '5rem',
           }}
         >
           <Button variant="primary">
